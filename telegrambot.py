842dce3e4c64cf5c977df9e1cad190022b25f64d
pranav3714 <51287393+pranav3714@users.noreply.github.com>
2019-10-19 18:43:38 +0530

    Genesis Commit

---
+++ telegrambot.py
@@ -0,0 +1,46 @@
+#coder :- Salman Faris
+
+import sys
+import time
+import telepot
+import RPi.GPIO as GPIO
+
+#LED
+def on(pin):
+        GPIO.output(pin,GPIO.HIGH)
+        return
+def off(pin):
+        GPIO.output(pin,GPIO.LOW)
+        return
+# to use Raspberry Pi board pin numbers
+GPIO.setmode(GPIO.BOARD)
+# set up GPIO output channel
+GPIO.setup(4, GPIO.OUT)#pin number 7
+
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+
+    print('Got command: %s' % command)
+
+    if command == 'on':
+       bot.sendMessage(chat_id, on(4))
+    elif command =='off':
+       bot.sendMessage(chat_id, off(4))
+
+bot = telepot.Bot('Bot Token')
+bot.message_loop(handle)
+print('I am listening...')
+
+while 1:
+    try:
+        time.sleep(10)
+    
+    except KeyboardInterrupt:
+        print('\n Program interrupted')
+        GPIO.cleanup()
+        exit()
+    
+    except:
+        print('Other error or exception occured!')
+        GPIO.cleanup()
